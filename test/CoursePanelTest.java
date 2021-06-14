2fafeb444b1107faa4651721c5110ad025eff56d
vnvj0033 <vnvj0033@gmail.com>
2021-06-14 15:00:55 +0900

    [red] 패널 테이블 확인 테스트

--- test/CoursePanelTest.java
+++ test/CoursePanelTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CoursePanelTest {
@@ -11,4 +13,13 @@ public class CoursePanelTest {
         assertEquals(panel.getName(), CoursePanel.NAME);
     }
 
+
+    @Test
+    void testTable() {
+        CoursePanel panel = new CoursePanel();
+
+        JTable table = (JTable)Util.getComponent(panel, CoursePanel.TABLE_NAME);
+
+        assertEquals(table.getName(), CoursePanel.TABLE_NAME);
+    }
 }
