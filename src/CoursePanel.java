9f9050fa64bdd23e74e4c6e43981786bd2403fe9
vnvj0033 <vnvj0033@gmail.com>
2021-06-14 15:16:28 +0900

    UI 구성

--- src/CoursePanel.java
+++ src/CoursePanel.java
@@ -3,8 +3,16 @@ import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
 import java.awt.*;
 
+import static java.awt.GridBagConstraints.*;
+import static java.awt.GridBagConstraints.HORIZONTAL;
+
 public class CoursePanel extends JPanel {
     public static final String NAME = "coursePanelName";
+
+    private JButton addButton = new JButton("Add");
+    private JTextField departmentTextField = new JTextField();
+    private JTextField searchField = new JTextField();
+    private JTable table = new JTable();
 
     CoursePanel() {
         setName(NAME);
@@ -15,19 +23,8 @@ public class CoursePanel extends JPanel {
         setLayout(new BorderLayout());
 
         createBorder();
+        createBottom();
         createTable();
-    }
-
-    private void createTable() {
-        JTable table = new JTable();
-
-        table.setShowGrid(false);
-        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
-        JScrollPane coursesScroll = new JScrollPane(table);
-        coursesScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
-        add(coursesScroll);
     }
 
     private void createBorder() {
@@ -36,5 +33,70 @@ public class CoursePanel extends JPanel {
         Border bevelBorder = BorderFactory.createBevelBorder(BevelBorder.RAISED);
         Border titleBorder = BorderFactory.createTitledBorder(bevelBorder, "Courses");
         setBorder(BorderFactory.createCompoundBorder(emptyBorder, titleBorder));
+    }
+
+    private void createTable() {
+        table.setShowGrid(false);
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        JScrollPane coursesScroll = new JScrollPane(table);
+        coursesScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        add(coursesScroll, BorderLayout.CENTER);
+    }
+
+    private void createBottom() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+
+        addButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JPanel field = createField();
+
+        JLabel searchLabel = new JLabel("Search");
+        searchLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        panel.add(Box.createRigidArea(new Dimension(0, 6)));
+        panel.add(addButton);
+        panel.add(Box.createRigidArea(new Dimension(0, 6)));
+        panel.add(field);
+        panel.add(Box.createRigidArea(new Dimension(0, 8)));
+        panel.add(searchLabel);
+        panel.add(searchField);
+
+        panel.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
+        add(panel, BorderLayout.SOUTH);
+    }
+
+
+    private JPanel createField() {
+        GridBagLayout layout = new GridBagLayout();
+
+        JPanel panel = new JPanel(layout);
+
+        JLabel label = new JLabel("Department");
+        departmentTextField.setColumns(20);
+
+        Insets insets = new Insets(3, 3, 3, 3);
+
+        layout.setConstraints(label, new GridBagConstraints(
+                0, 0,
+                1, 1,
+                40, 1,
+                LINE_END, NONE,
+                insets,
+                0, 0));
+        layout.setConstraints(departmentTextField, new GridBagConstraints(
+                1, 0,
+                2, 1,
+                60, 1,
+                CENTER, HORIZONTAL,
+                insets,
+                0, 0));
+
+        panel.add(label);
+        panel.add(departmentTextField);
+
+        return panel;
     }
 }
--- test/CoursePanelTest.java
+++ test/CoursePanelTest.java
@@ -1,7 +1,5 @@
 import org.junit.jupiter.api.Test;
 
-import javax.swing.*;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CoursePanelTest {
@@ -11,15 +9,5 @@ public class CoursePanelTest {
         CoursePanel panel = new CoursePanel();
 
         assertEquals(panel.getName(), CoursePanel.NAME);
-    }
-
-
-    @Test
-    void testTable() {
-        CoursePanel panel = new CoursePanel();
-
-        JTable table = (JTable)Util.getComponent(panel, CoursePanel.TABLE_NAME);
-
-        assertEquals(table.getName(), CoursePanel.TABLE_NAME);
     }
 }
