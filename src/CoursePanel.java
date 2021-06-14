4260259f17b71258482e5a099644ede791048905
vnvj0033 <vnvj0033@gmail.com>
2021-06-14 14:27:37 +0900

    [green] 패널 이름 생성

---
+++ src/CoursePanel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class CoursePanel extends JPanel {
+    public static final String NAME = "coursePanelName";
+
+    public String getName() {
+        return NAME;
+    }
+}
