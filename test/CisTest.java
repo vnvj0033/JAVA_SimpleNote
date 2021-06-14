9099c4249e7e1184d335c1be7305b461574fa091
vnvj0033 <vnvj0033@gmail.com>
2021-06-14 13:50:34 +0900

    [red] create Cis test

---
+++ test/CisTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CisTest {
+
+    @Test
+    void testCreate() {
+        Cis cis = new Cis();
+        JFrame frame = cis.getFrame();
+
+        assertEquals(frame.getTitle(), CIS.TITLE);
+    }
+}
