5ec060381e5d116b082917b807f1c5c0b87e94a1
vnvj0033 <vnvj0033@gmail.com>
2021-06-14 13:46:53 +0900

    [blue] Course new feat setDepartment

--- src/Course.java
+++ src/Course.java
@@ -1,9 +1,12 @@
 public class Course {
 
+    private String department;
+
     public Course(String department) {
+        this.department = department;
     }
 
     public String getDepartment() {
-        return "Tdd";
+        return department;
     }
 }
