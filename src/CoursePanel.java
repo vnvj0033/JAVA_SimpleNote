c671b032c8b5f839c329c4853e07a23b023f0795
vnvj0033 <vnvj0033@gmail.com>
2021-06-14 14:34:23 +0900

    Panel setBorder

--- src/CoursePanel.java
+++ src/CoursePanel.java
@@ -1,9 +1,23 @@
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class CoursePanel extends JPanel {
     public static final String NAME = "coursePanelName";
 
     CoursePanel() {
         setName(NAME);
+        createLayout();
+    }
+
+    private void createLayout() {
+        setLayout(new BorderLayout());
+
+        final int pad = 6;
+        Border emptyBorder = BorderFactory.createEmptyBorder(pad, pad, pad, pad);
+        Border bevelBorder = BorderFactory.createBevelBorder(BevelBorder.RAISED);
+        Border titleBorder = BorderFactory.createTitledBorder(bevelBorder, "Courses");
+        setBorder(BorderFactory.createCompoundBorder(emptyBorder, titleBorder));
     }
 }
