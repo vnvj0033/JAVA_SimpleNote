f57db7b6c49e467e804aaba8a2a15fc17a47777f
vnvj0033 <vnvj0033@gmail.com>
2021-06-18 13:13:07 +0900

    [blue] Cis test 정리
    프레임 show, before 설정

--- test/CisTest.java
+++ test/CisTest.java
@@ -1,20 +1,34 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CisTest {
+    Cis cis;
+    JFrame frame;
+
+    @BeforeEach
+    void setUp() {
+        cis = new Cis();
+        frame = cis.getFrame();
+    }
 
     @Test
     void testCreate() {
-        Cis cis = new Cis();
-        JFrame frame = cis.getFrame();
-
         assertEquals(frame.getTitle(), Cis.TITLE);
-
+        assertEquals(frame.getDefaultCloseOperation(), JFrame.DISPOSE_ON_CLOSE);
         assertEquals(frame.getWidth(), Cis.WIDTH);
         assertEquals(frame.getHeight(), Cis.HEIGHT);
     }
 
+    @Test
+    void testShow() {
+        cis.show();
+        frame.setVisible(true);
+
+        assertTrue(frame.isVisible());
+    }
 }
